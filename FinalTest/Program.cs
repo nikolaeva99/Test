f6411e2056d8f4a1c50d4d390cc902551f4e8a5b
Anastasia <nsta_nikolaeva@inbox.ru>
2022-06-19 20:07:40 +0300

    First trying to solve the task

--- FinalTest/Program.cs
+++ FinalTest/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿string CreateArray (int size )
+{
+    string[] array = new string[size];
+    
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(10, 100);
+    }
+    return array;
+}
+
+void ShowArray (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+string SolveTask (string[] array)
+{
+    int size = 0;
+    string[] newArray = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] / 100 < 10)
+        {
+            newArray[i] = array[i];
+            size += 1;
+        }
+    }
+    return newArray;
+}
+
+Console.Write("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] myArray = CreateArray(size);
+ShowArray(myArray);
+string[] reNewArray = SolveTask(myArray);
+ShowArray(reNewArray);
